69dbb35d42e2dafb4995926b8b9f08f53033c80e
Andrews Selvaraj <andrewsselvaraj@gmail.com>
2025-12-04 22:51:11 +0530

    aaaa

--- SpringBootConfig/src/test/java/com/config/SpringBootConfigApplicationTests.java
+++ SpringBootConfig/src/test/java/com/config/SpringBootConfigApplicationTests.java
@@ -3,10 +3,10 @@ package com.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringBootConfigApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
